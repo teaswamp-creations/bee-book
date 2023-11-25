c36dd5b5083a826d5fa4ce564d27ffe63570f111
harrig12 <cait.harrigan@mail.utoronto.ca>
2023-11-25 05:43:22 -0500

    gbif map on pages

--- generate_pages.R
+++ generate_pages.R
@@ -6,20 +6,23 @@ library(googlesheets4)
 gs4_auth('galiwatch.info@gmail.com')
 df <- read_sheet("1c4N54O_x8a_Wfoe3tct0gryId2wG0ze-HA-Ckms4dy4", sheet = 'Bee families of BC') %>%
   mutate(`Image link` = replace_na(`Image link`, '/files/no_bee.png'))
-# generate species data
+
+
+# make species page
 make_page <- function(row){
   parent <- gsub(' ', '_', paste0(tolower(row['Genus']),'_', tolower(row['Species'])))
   if (!dir.exists(file.path('bees', parent))){dir.create(file.path('bees', parent))}
   contents <- paste0(
     '---\ntitle: ',
     row['Genus'], ' ', row['Species'],
+    '\nsubtite: ', row['Common name'], 
     '\nengine: knitr',
     '\ncategories:\n  - ', row['Family'], 
     '\nfreeze: True',
     '\n---\n',
-    '(', row['Common name'], ')\n\n',
-    '[![`r emo::ji("copyright")` ', row['Observer'], '. ', row['Observation date'], '.](', row['Image link'], '){height=300}](', row['Observation Link'], ')'
-  )
+    '[![`r emo::ji("copyright")` ', row['Observer'], '. ', row['Observation date'], '.](', row['Image link'], '){height=300}](', row['Observation Link'], ')',
+    '\n\n`r source("../../page_functions.R"); make_species_map("', row["Species"], '")`'
+    )
   write(contents, file.path('bees', parent, 'index.qmd'))
 }
 
---
+++ page_functions.R
@@ -0,0 +1,39 @@
+library(plotly)
+library(tidyverse)
+
+
+# make species map
+gbif <- read_delim("../../files/0004981-231120084113126.csv", delim='\t') %>%
+  mutate(date = ymd(date(eventDate)), Year = year(date),
+         Month = lubridate::month(date, label=T),
+         lat=decimalLatitude, lon=decimalLongitude) %>%
+  drop_na(date, lat, lon, genus, occurrenceID) %>%
+  separate_wider_delim(species, ' ', names=c('Genus', 'Species'), too_many = 'merge')
+
+
+
+make_species_map <- function(species){
+  if (!(species %in% gbif$Species)){
+    return("no GBIF data to display")
+  }
+  
+  gbif %>% 
+    filter(Species == species) %>%
+    plot_ly() %>%
+    add_trace(
+      lat = ~lat, lon =~lon,
+      mode = 'markers', type = 'scattermapbox',
+      color = ~Year,
+      text = ~paste('Species:', Genus, Species, '\nObserved:', stamp("March 1, 1999")(date)),
+      size=10,
+      opacity=1,
+      colors='Blues',
+      name=""
+    ) %>%
+    layout(mapbox = list(
+      style = 'carto-positron', show_legend = T,
+      #zoom=7, center = list(lon = -123, lat = 49)),
+      zoom=3.8, center = list(lon = -127, lat = 55)),
+      title = 'GBIF observations') %>%
+    return()
+}
