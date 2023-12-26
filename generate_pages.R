3bd1029446b83eaff86f992ef7aeea6d57fac832
harrig12 <cait.harrigan@mail.utoronto.ca>
2023-12-26 14:56:26 -0800

    new front page, summary table and nodes

--- generate_pages.R
+++ generate_pages.R
@@ -9,7 +9,7 @@ df <- read_sheet("1c4N54O_x8a_Wfoe3tct0gryId2wG0ze-HA-Ckms4dy4", sheet = 'Bee fa
 
 
 # make species map
-gbif <- read_delim("../../files/0004981-231120084113126.csv", delim='\t') %>%
+gbif <- read_delim("../../files/0037810-231120084113126.csv", delim='\t') %>%
   mutate(date = ymd(date(eventDate)), Year = year(date),
          Month = lubridate::month(date, label=T),
          lat=decimalLatitude, lon=decimalLongitude) %>%
@@ -29,9 +29,15 @@ make_page <- function(row){
     '\ncategories:\n  - ', row['Family'], 
     '\nfreeze: auto',
     '\n---\n',
-    '[![`r emo::ji("copyright")` ', row['Observer'], '. ', row['Observation date'], '.](', row['Image link'], '){height=300}](', row['Observation Link'], ')',
-    ifelse(row[['Species']] %in% gbif$Species, paste0('\n\n```{r warning=F, message=F, echo=F}\nsource("../../page_functions.R")\nmake_species_map("', row["Species"], '")\n```'),
-           '\n\nNo GBIF data to display')
+    '![`r emo::ji("copyright")` ', row['Observer'], '.](', row['Image link'], '){height=300}', 
+    '\n\n', ifelse(!is.na(row['Summary note']), row['Summary note'], ''), 
+    '\n\n', ifelse(!(row['Species'] %in% gbif$Species), 'No GBIF observations in Pacific Maritime to display.',
+                   paste0('\n\n```{r warning=F, message=F, echo=F}\nsource("../../page_functions.R")\nmake_species_map("', row["Species"], '")\n```')
+                   ),
+    '\n<br><br>\n<center>\n',
+    '\n\n<a href="', row['Map Link'], '"><button type="button" class="btn btn-secondary">See all ', row['Genus'], ' ', row['Species'], 
+    ' in BC <i class="fa-solid fa-arrow-up-right-from-square"></i> </button></a>',
+    '\n</center>'
     )
   write(contents, file.path('bees', parent, 'index.qmd'))
 }
